0ecfa7752e21913a0c76634211f38ef91dded496
Mary739-1 <103171761+Mary739-1@users.noreply.github.com>
2023-03-07 10:06:05 +0700

    Add files via upload

---
+++ lr1/test2.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "Разработчик: Шуваркова Мария"
+echo "Программа поиска информации о файле"
+echo "С помощью данной программы вы можете просматривать временные метки файла"
+filename=""
+choice="yes"
+
+while true
+do
+	echo "Введите имя файла:"
+	read filename
+	if [ $filename ] 
+		then
+			echo ""
+			echo "Время последнего доступа: "; stat -c %x $filename
+			echo "Время последнего изменения: "; stat -c %y $filename
+			echo "Время изменения индексного дескриптора: "; stat -c %z $filename
+			echo ""		
+		else 
+			echo "Ошибка! Файл не найден" >/dev/stderr
+			echo ""
+	fi
+	while true
+    	do
+		echo "Хотите попробовать еще раз? (y/n)"
+		read choice
+		case "$choice" in
+				"Y" | "y" | "yes" | "Yes") echo "";  break;;
+				"N" | "n" | "no" | "No") echo ""; echo "Выход из программы..."; exit $?;;
+				* ) echo "Ошибка. Нажмите y или n" >/dev/stderr;;
+            	esac
+    	done
+done
